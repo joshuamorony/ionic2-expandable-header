9b941ad107279a7621373364094fb186bf61f60f
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-03-19 13:19:05 +1030

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	testData: any = new Array(5);
+
+	constructor(public navCtrl: NavController) {
+
+	}
+
+}
